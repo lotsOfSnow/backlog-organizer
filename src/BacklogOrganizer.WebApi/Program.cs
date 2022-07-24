e494c085bf00c3a66e4e6c44c0f8550fc41f6a5e
lotsOfSnow <28030565+lotsOfSnow@users.noreply.github.com>
2022-07-24 18:12:22 +0200

    Install all modules and dependencies in API project (#11)
    * Load all modules and dependencies in main project
    
    * Remove extra line

--- src/BacklogOrganizer.WebApi/Program.cs
+++ src/BacklogOrganizer.WebApi/Program.cs
@@ -1,13 +1,12 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+using BacklogOrganizer.WebApi;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDependencies(builder.Configuration);
 
-WebApplication app = builder.Build();
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
