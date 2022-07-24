d74c13176fef303265afdc52eb0edbd480c3f52c
lotsofsnow <snowlotsof@gmail.com>
2022-07-24 17:52:12 +0200

    Load all modules and dependencies in main project

---
+++ src/BacklogOrganizer.WebApi/Installer.cs
@@ -0,0 +1,20 @@
+using BacklogOrganizer.Shared.Infrastructure;
+using BacklogOrganizer.Shared.Infrastructure.Modularization;
+
+namespace BacklogOrganizer.WebApi;
+
+internal static class Installer
+{
+    public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSharedInfrastructure();
+        ModuleLoader.Load(services, configuration, AppDomain.CurrentDomain.GetAssemblies());
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+
+        return services;
+    }
+}
