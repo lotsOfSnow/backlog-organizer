81b2c83768abd0f9e8a86d194fdae6aef0f7c459
lotsofsnow <snowlotsof@gmail.com>
2022-07-24 21:36:04 +0200

    Seed single gaming backlog

--- src/Modules/Backlogs/Backlogs.Core/Backlogs/Gaming/GamingBacklog.cs
+++ src/Modules/Backlogs/Backlogs.Core/Backlogs/Gaming/GamingBacklog.cs
@@ -2,4 +2,5 @@ namespace BacklogOrganizer.Modules.Backlogs.Core.Backlogs.Gaming;
 
 public class GamingBacklog : Backlog<GameBacklogItem>
 {
+    public static readonly Guid InstanceId = new("6c24c264-c53d-4f44-adc4-26560e790a73");
 }
