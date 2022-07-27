e52f964d3a146dcfc54af693a8ae64c2c6be9f0c
lotsofsnow <snowlotsof@gmail.com>
2022-07-27 20:57:47 +0200

    Load all modules before anything else

--- src/BacklogOrganizer.WebApi/Installer.cs
+++ src/BacklogOrganizer.WebApi/Installer.cs
@@ -9,12 +9,12 @@ internal static class Installer
 {
     public static WebApplicationBuilder AddDependencies(this WebApplicationBuilder builder)
     {
+        ModuleLoader.Load(builder.Services, builder.Configuration);
+
         builder.Services.AddSharedCore();
         builder.Services.AddSharedInfrastructure();
 
         builder.Logging.UseSerilog(builder.Services, builder.Configuration);
-
-        ModuleLoader.Load(builder.Services, builder.Configuration);
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
