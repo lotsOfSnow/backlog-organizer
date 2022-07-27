c8f9f2229d616b41e22034a71521c861ef6ce560
lotsofsnow <snowlotsof@gmail.com>
2022-07-27 22:14:34 +0200

    Inject validators

---
+++ src/Shared/Shared.Infrastructure/Validation/FluentValidationExtensions.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BacklogOrganizer.Shared.Infrastructure.Validation;
+
+public static class FluentValidationExtensions
+{
+    public static IServiceCollection AddValidators(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+
+        return services;
+    }
+}
