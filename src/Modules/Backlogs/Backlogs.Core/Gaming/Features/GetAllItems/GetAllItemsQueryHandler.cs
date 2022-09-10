68ccd81d53e9e8aba675c778b89d6f5c8c09d8b7
lotsOfSnow <snowlotsof@gmail.com>
2022-09-10 18:07:20 +0200

    Add query to get all backlog items

---
+++ src/Modules/Backlogs/Backlogs.Core/Gaming/Features/GetAllItems/GetAllItemsQueryHandler.cs
@@ -0,0 +1,40 @@
+using BacklogOrganizer.Modules.Backlogs.Core.Data;
+using BacklogOrganizer.Shared.Core.Results;
+using BacklogOrganizer.Shared.Core.Results.Errors;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BacklogOrganizer.Modules.Backlogs.Core.Gaming.Features.GetAllItems;
+
+public class GetAllItemsQueryHandler : IRequestHandler<GetAllItemsQuery, Result<IEnumerable<GameBacklogItemDto>>>
+{
+    private readonly IBacklogStorage _storage;
+    private readonly ILogger<GetAllItemsQueryHandler> _logger;
+
+    public GetAllItemsQueryHandler(IBacklogStorage storage, ILogger<GetAllItemsQueryHandler> logger)
+    {
+        _storage = storage;
+        _logger = logger;
+    }
+
+    public async Task<Result<IEnumerable<GameBacklogItemDto>>> Handle(GetAllItemsQuery request, CancellationToken cancellationToken)
+    {
+        var backlog = await _storage.GamingBacklogs
+            .Include(x => x.Items)
+            .FirstOrDefaultAsync(x => x.Id == request.BacklogId, cancellationToken);
+
+        if (backlog is null)
+        {
+            _logger.LogInformation("No backlog with Id {RequestBacklogId} found", request.BacklogId);
+            return Result<IEnumerable<GameBacklogItemDto>>.Failure(new(ErrorReason.ResourceNotFound, $"Backlog with Id {request.BacklogId} doesn't exist"));
+        }
+
+        var mappedItems = backlog
+            .Items
+            .Select(x => new GameBacklogItemDto(x.Id, x.Name))
+            .ToList();
+
+        return Result<IEnumerable<GameBacklogItemDto>>.Success(mappedItems);
+    }
+}
---
+++ tests/Modules/Backlogs/Backlogs.Core.IntegrationTests/Gaming/Features/GetAllItemsTests.cs
@@ -0,0 +1,48 @@
+using BacklogOrganizer.Modules.Backlogs.Core.Gaming;
+using BacklogOrganizer.Modules.Backlogs.Core.Gaming.Features.AddItem;
+using BacklogOrganizer.Modules.Backlogs.Core.Gaming.Features.GetAllItems;
+using BacklogOrganizer.Shared.Api.IntegrationTests.Assertions;
+using BacklogOrganizer.Shared.Core.Results.Errors;
+using Xunit.Abstractions;
+
+namespace BacklogOrganizer.Modules.Backlogs.Core.IntegrationTests.Gaming.Features;
+
+public class GetAllItemsTests : IClassFixture<BacklogsApplicationFactory>
+{
+    private readonly BacklogsApplicationFactory _factory;
+
+    public GetAllItemsTests(ITestOutputHelper testOutputHelper, BacklogsApplicationFactory factory)
+    {
+        _factory = factory;
+        _factory.TestOutputHelper = testOutputHelper;
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    public async Task Gets_all_items(int itemsToCreate)
+    {
+        var query = new GetAllItemsQuery(GamingBacklog.InstanceId);
+        for (var i = 0; i < itemsToCreate; i++)
+        {
+            var creationCommand = new AddBacklogItemCommand(GamingBacklog.InstanceId, "Name");
+            await _factory.SendAsync(creationCommand);
+        }
+
+        var response = await _factory.SendAsync(query);
+
+        response.Should().BeSuccessful();
+        response.Value.Should().HaveCount(itemsToCreate);
+    }
+
+    [Fact]
+    public async Task Handles_wrong_backlog_id()
+    {
+        var nonExistingId = Guid.Parse("4de89043-5550-4ead-954d-d28488993f4d");
+        var query = new GetAllItemsQuery(nonExistingId);
+
+        var response = await _factory.SendAsync(query);
+
+        response.Should().HaveError(ErrorReason.ResourceNotFound);
+    }
+}
