917b5259e8f2445f508c8b7200023625fc08b5cb
lotsOfSnow <snowlotsof@gmail.com>
2022-09-18 16:13:56 +0200

    Improve result object

--- src/Shared/Shared.Core/Results/Result.cs
+++ src/Shared/Shared.Core/Results/Result.cs
@@ -1,12 +1,28 @@
+using System.Diagnostics.CodeAnalysis;
 using BacklogOrganizer.Shared.Core.Results.Errors;
 
 namespace BacklogOrganizer.Shared.Core.Results;
 
-public record Result<T>(bool IsSuccess, T? Value, ResultError? Error)
+public sealed class Result<T>
 {
+    [MemberNotNullWhen(true, nameof(Result<T>.Value))]
+    [MemberNotNullWhen(false, nameof(Result<T>.Error))]
+    public bool IsSuccess { get; }
+
+    public T? Value { get; }
+
+    public ResultError? Error { get; }
+
     public static Result<T> Success(T value)
         => new(true, value, null);
 
     public static Result<T> Failure(ResultError error)
         => new(false, default, error);
+
+    private Result(bool isSuccess, T? value, ResultError? error)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        Error = error;
+    }
 }
