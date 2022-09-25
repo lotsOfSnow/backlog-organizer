9c6daa2d5354d528c056fe3b4d9386833dcade0d
lotsOfSnow <snowlotsof@gmail.com>
2022-09-25 20:15:49 +0200

    Use query marker interface

--- src/Modules/Backlogs/Backlogs.Core/Gaming/Groups/Features/GetItems/GetItemsQuery.cs
+++ src/Modules/Backlogs/Backlogs.Core/Gaming/Groups/Features/GetItems/GetItemsQuery.cs
@@ -1,9 +1,10 @@
-﻿using BacklogOrganizer.Shared.Core.Results;
+﻿using BacklogOrganizer.Shared.Core.Mediator;
+using BacklogOrganizer.Shared.Core.Results;
 using MediatR;
 
 namespace BacklogOrganizer.Modules.Backlogs.Core.Gaming.Groups.Features.GetItems;
 
-public record GetItemsQuery(Guid GroupId) : IRequest<Result<IEnumerable<GroupAssignmentDto>>>;
+public record GetItemsQuery(Guid GroupId) : IQuery<Result<IEnumerable<GroupAssignmentDto>>>;
 
 public class GetItemsQueryHandler : IRequestHandler<GetItemsQuery, Result<IEnumerable<GroupAssignmentDto>>>
 {
