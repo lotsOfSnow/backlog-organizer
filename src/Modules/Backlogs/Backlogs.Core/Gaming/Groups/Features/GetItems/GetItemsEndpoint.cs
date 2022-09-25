fdd5e517149cac26ace4532b1a3947b43ab88ed0
lotsOfSnow <snowlotsof@gmail.com>
2022-09-25 20:34:51 +0200

    Add endpoint

---
+++ src/Modules/Backlogs/Backlogs.Core/Gaming/Groups/Features/GetItems/GetItemsEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Mime;
+using BacklogOrganizer.Modules.Backlogs.Core.Api;
+using BacklogOrganizer.Shared.Api.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace BacklogOrganizer.Modules.Backlogs.Core.Gaming.Groups.Features.GetItems;
+
+[Route(ApiRoutes.BacklogGroups + "/{groupId}/items")]
+[ApiVersion(ApiVersions.V1)]
+public class GetItemsEndpoint : BaseController
+{
+    public GetItemsEndpoint(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet]
+    [SwaggerOperation(
+        Summary = "Get items assigned to group",
+        OperationId = "getAssignmentsOfGamingBacklogGroup",
+        Tags = new[] { ApiTags.BacklogGroups })]
+    [Consumes(MediaTypeNames.Application.Json)]
+    public async Task<ActionResult<IEnumerable<GroupAssignmentDto>>> GetAssignmentsAsync(Guid backlogId, Guid groupId)
+    {
+        var command = new GetItemsQuery(groupId);
+        var result = await Mediator.Send(command);
+        return result.ToObjectResult();
+    }
+}
