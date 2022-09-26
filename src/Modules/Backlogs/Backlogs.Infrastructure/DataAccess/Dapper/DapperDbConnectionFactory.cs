925b161e90dcb8f7c405d2f58772da3c627266ba
lotsOfSnow <snowlotsof@gmail.com>
2022-09-26 15:59:36 +0200

    Implement IDisposable

--- src/Modules/Backlogs/Backlogs.Infrastructure/DataAccess/Dapper/DapperDbConnectionFactory.cs
+++ src/Modules/Backlogs/Backlogs.Infrastructure/DataAccess/Dapper/DapperDbConnectionFactory.cs
@@ -5,9 +5,11 @@ using Microsoft.Extensions.Options;
 using Npgsql;
 
 namespace BacklogOrganizer.Modules.Backlogs.Infrastructure.DataAccess.Dapper;
-public class DapperDbConnectionFactory : IQueryDbConnectionFactory
+public class DapperDbConnectionFactory : IQueryDbConnectionFactory, IDisposable
 {
     private readonly string _connectionString;
+
+    private IDbConnection? _connection;
 
     public DapperDbConnectionFactory(IOptions<PostgresOptions> postgresOptions)
     {
@@ -18,6 +20,28 @@ public class DapperDbConnectionFactory : IQueryDbConnectionFactory
     {
         var connection = new NpgsqlConnection(_connectionString);
         await connection.OpenAsync();
+
+        _connection = connection;
+
         return connection;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_connection is null)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            _connection.Dispose();
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 }
