32f34ec486ffac467b4b2fa7295ea3fefeb99e45
lotsOfSnow <snowlotsof@gmail.com>
2022-09-26 18:03:06 +0200

    Rename variable

--- src/Modules/Backlogs/Backlogs.Core/Gaming/Groups/Features/GetItems/GetAssignmentsQuery.cs
+++ src/Modules/Backlogs/Backlogs.Core/Gaming/Groups/Features/GetItems/GetAssignmentsQuery.cs
@@ -20,27 +20,26 @@ public class GetAssignmentsQueryHandler : IRequestHandler<GetAssignmentsQuery, R
 
     public async Task<Result<IEnumerable<GroupAssignmentDto>>> Handle(GetAssignmentsQuery request, CancellationToken cancellationToken)
     {
-        var conn = await _queryRepository.ConnectionFactory.GetOrCreateConnectionAsync();
+        var dbConnection = await _queryRepository.ConnectionFactory.GetOrCreateConnectionAsync();
 
         var backlogQueryArgs = new { request.BacklogId };
         var backlogExistenceQuery = _queryRepository.GetExistenceCheckQuery(OrmMappings.Backlogs.Table, OrmMappings.Backlogs.Columns.Id, nameof(backlogQueryArgs.BacklogId));
-        var backlogExists = await conn.QuerySingleOrDefaultAsync<bool>(backlogExistenceQuery, backlogQueryArgs);
+        var backlogExists = await dbConnection.QuerySingleOrDefaultAsync<bool>(backlogExistenceQuery, backlogQueryArgs);
         if (!backlogExists)
         {
             return Result<IEnumerable<GroupAssignmentDto>>.Failure(BacklogResultErrors.GetBacklogNotFoundError(request.BacklogId));
         }
 
         var groupQueryArgs = new { request.GroupId };
-
         var groupExistenceQuery = _queryRepository.GetExistenceCheckQuery(OrmMappings.Groups.Table, OrmMappings.Groups.Columns.Id, nameof(groupQueryArgs.GroupId));
-        var groupExists = await conn.QuerySingleOrDefaultAsync<bool>(groupExistenceQuery, groupQueryArgs);
+        var groupExists = await dbConnection.QuerySingleOrDefaultAsync<bool>(groupExistenceQuery, groupQueryArgs);
         if (!groupExists)
         {
             return Result<IEnumerable<GroupAssignmentDto>>.Failure(BacklogResultErrors.GetGroupNotFoundError(request.BacklogId, request.GroupId));
         }
 
         var assignmentsQuery = _queryRepository.GetQuery("SELECT * FROM {0} WHERE {1} = @GroupId", OrmMappings.GroupAssignments.Table, OrmMappings.GroupAssignments.Columns.GroupId);
-        var result = await conn.QueryAsync<GroupAssignmentDto>(assignmentsQuery, groupQueryArgs);
+        var result = await dbConnection.QueryAsync<GroupAssignmentDto>(assignmentsQuery, groupQueryArgs);
         return Result<IEnumerable<GroupAssignmentDto>>.Success(result);
     }
 }
